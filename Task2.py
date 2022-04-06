1cc780476820a2bb7fe519f984709b7fa5085d1b
0mar228 <103130496+0mar228@users.noreply.github.com>
2022-04-06 18:53:52 +0300

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,9 @@
+def mathhelper():
+    q = int(input('радиус'))
+    e = 3.14 * (q * q)
+    print('Площадь:', e)
+    a = q*2
+    print('Диаметр:', a)
+    c = 2*3.14*q
+    print('Длина окружности:', c)
+mathhelper()
