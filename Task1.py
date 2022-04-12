5d456dbdc4d06f3d2d4c1e8b884d111f181c963b
0mar228 <103130496+0mar228@users.noreply.github.com>
2022-04-12 20:16:04 +0300

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,6 +1,7 @@
-def mathhelper():
-    q = int(input('1 катет'))
-    w = int(input('2 катет'))
-    e = q*q + w*w
-    print('Гипотенуза:', e)
-mathhelper()
+import math
+q = int(input('1 катет'))
+w = int(input('2 катет'))
+e = q**2 + w**2
+d = math.sqrt(q**2 + w**2)
+v = round(d,3)
+print('Гипотенуза:', v)
